4c9e553c498c4e2e9c7893dcf96ddc6b9f4317ed
Siah Monk <dwaynemhinds@gmail.com>
2018-08-11 23:40:59 -0400

    Updating connection info for Heroku

--- config/connnection.js
+++ config/connnection.js
@@ -3,9 +3,9 @@ var mysql = require("mysql");
 
 //Heroku JawsDB connection setup
 if (process.env.JAWSDB_URL) {
-  let connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  var connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "siahDB",
