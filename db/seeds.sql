c7b539f29ce4614942c6dd27298ad71e42ff72e4
Siah Monk <dwaynemhinds@gmail.com>
2018-08-11 22:43:50 -0400

    Adding repo structure and all files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers_db.burgers (burger_name) VALUES ('Tennesse Bourbon Burger');
+INSERT INTO burgers_db.burgers (burger_name) VALUES ('Chili Cheeseburger');
+INSERT INTO burgers_db.burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers_db.burgers (burger_name, devoured) VALUES ('Sweet Island Burger', true);
+INSERT INTO burgers_db.burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers_db.burgers (burger_name) VALUES ('Cowboy Burger');
