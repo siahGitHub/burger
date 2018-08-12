94f9993fdf4eb92dfc328ca27b09b5773d677eed
Siah Monk <dwaynemhinds@gmail.com>
2018-08-11 23:08:13 -0400

    adding Heroku JawsDB connection info

--- config/connnection.js
+++ config/connnection.js
@@ -1,14 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "siahDB",
-  password: "siahDB#1623",
-  database: "burgers_db"
-});
-
+//Heroku JawsDB connection setup
+if (process.env.JAWSDB_URL) {
+  let connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "siahDB",
+    password: "siahDB#1623",
+    database: "burgers_db"
+  });
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
